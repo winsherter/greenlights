56328c62c333f409a4834c208c75924916033aaa
Winsherter <gilbertabotchi@gmail.com>
2024-05-15 12:18:51 +0000

    First Commit

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func (app *application) readIDParam(r, *http.Request) (int64, error) {
+func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
